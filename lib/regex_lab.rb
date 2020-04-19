0520f34458d2d20d18c49c4c8241f2b2108c254d
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-19 20:05:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-puts word.scan(/\A[a,e,i,o,u]/)
+word.scan(/\A[a,e,i,o,u]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -18,4 +18,4 @@ def valid_phone_number?(phone)
 
 end
 
-starts_with_a_vowel?("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+puts starts_with_a_vowel?("Test")
